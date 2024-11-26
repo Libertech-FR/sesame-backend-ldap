793350fdca5e5273db11234a081ff371bf9a2c71
Alain Abbas <alain.abbas@libertech.fr>
2024-11-26 09:19:26 +0100

    Update backend_ldap_utils.py

--- src/lib/backend_ldap_utils.py
+++ src/lib/backend_ldap_utils.py
@@ -1,5 +1,7 @@
 import configparser
 import json
+
+import jinja2
 import ldap
 import ldap.modlist as modlist
 from sys import stdin
@@ -16,7 +18,12 @@ def config(key,default=''):
     c=__CONFIG__['config']
     return c.get(key,default)
 
-
+def get_config():
+    items=__CONFIG__.items('config')
+    data = {}
+    for k, v in items:
+        data[k] = v
+    return data
 def connect_ldap(url,dn,password):
     try:
         l=ldap.initialize(url)
@@ -77,33 +84,60 @@ def make_entry_array(entity):
         additionalFields = entity['payload']['additionalFields']['attributes']
     #inetOrgPerson
     for k,v in inetOrgPerson.items():
-        data[k]=str(v)
+        if type(v) is int:
+            v=str(v)
+        data[k.lower()]=v
 
     for obj in objectclasses:
         #recherche si l objectclass est exclu
         exclusions=config('excludedObjectclasses').lower()
         if exclusions.find(obj.lower()) == -1:
-            for k,v in additionalFields[obj].items():
-                data[k]=str(v)
+            if obj in additionalFields.keys():
+                for k,v in additionalFields[obj].items():
+                    if type(v) is int:
+                        v = str(v)
+                    data[k.lower()]=v
     return data
 
 
-def make_objectclass(entity):
+def make_objectclass(entity,entry):
     data = {}
-    if "identity" in entity['payload']:
-        objectclasses = entity['payload']['identity']['identity']['additionalFields']['objectClasses']
+    objectclasses=[]
+    if entry:
+        current=entry[0][1]['objectClass']
+        for k in current:
+            x=k.decode('UTF-8').lower()
+            objectclasses.append(x)
     else:
-        objectclasses = entity['payload']['additionalFields']['objectClasses']
+        objectclasses.append('top')
+        objectclasses.append('inetOrgPerson')
+    if "identity" in entity['payload']:
+        new_objectclasses = entity['payload']['identity']['identity']['additionalFields']['objectClasses']
+    else:
+        new_objectclasses = entity['payload']['additionalFields']['objectClasses']
 
-    return ['top', 'inetOrgPerson'] + objectclasses
+    for k in new_objectclasses:
+        if k.lower() not in objectclasses:
+            objectclasses.append(k)
+
+    return objectclasses
 
 
 def make_entry_array_without_empty(entity):
     data={}
     data1=make_entry_array(entity)
     for k,v in data1.items():
-        if str(v) != "":
-            data[k]=v
+        if type(v) is list and len(k) > 0:
+            l=[]
+            for i in v:
+                if i != '':
+                    l.append(i)
+            if len(l)>0:
+                data[k.lower()] = l
+        if type(v) is int:
+            v=str(v)
+        if type(v) is str and str(v) != "":
+            data[k.lower()]=v
     return data
 
 def convert_to_utf8(entry):
@@ -121,27 +155,35 @@ def compose_rdn(entity):
     rdn = config('rdnAttribute', 'uid')
     rdnValue = find_key(entity, rdn)
     return rdn +'=' + rdnValue
-
 def compose_dn(entity):
-    rdn=config('rdnAttribute','uid')
+    """Compose the DN of a identity"""
+    rdn = config('rdnAttribute', 'uid')
     rdnValue=find_key(entity,rdn)
+    data = {
+        'e': make_entry_array(entity),
+        'config': get_config(),
+        'rdnValue': rdnValue
+    }
+
+    x=type(rdnValue)
+    if rdnValue is None:
+        rdnValue='test'
     branchAttr=config('branchAttr','')
-    branch  = ''
+    data['rdnValue']=rdnValue
     if branchAttr != '':
         branchValue=find_key(entity,branchAttr)
-
-        match branchValue:
-            case 'etd':
-                branch=config('branchForEtd','')
-            case 'esn':
-                branch = config('branchForEsn', '')
-            case 'adm':
-                branch = config('branchForAdm', '')
-    if branch != '':
-        return rdn+ '=' + rdnValue+ ',' + branch + "," + config('userbase')
+        key_branch='branchFor' + branchValue
+        branch=config(key_branch,'')
+        data['branch']=branch
+        template_string = '{{ config.rdnattribute}}={{rdnValue}},{{ branch }},{{ config.userbase }}'
     else:
-        return rdn+ '=' + rdnValue+  "," + config('userbase')
-
+        if config('userbase','') != '':
+            template_string ='{{config.rdnattribute}}={{ rdnValue}},{{ config.userbase }}'
+        else:
+            template_string = '{{config.rdnattribute}}={{ rdnValue}},{{ config.base }}'
+    template = jinja2.Environment(loader=jinja2.BaseLoader()).from_string(config('dnTemplate',template_string))
+    content = template.render(data)
+    return content
 def dn_superior(dn):
    tab=dn.split(',')
    tab.pop(0)
@@ -153,13 +195,22 @@ def search_entity(l,entity):
     employeeNumber=find_key(entity,'employeeNumber')
     employeeType=find_key(entity,'employeeType')
     base=config('base')
-    filter='(&(employeeNumber=' + employeeNumber + ')(employeeType=' + employeeType + '))'
+    if config('searchFilter',''):
+        data = {
+            'e': make_entry_array(entity),
+        }
+        template = jinja2.Environment(loader=jinja2.BaseLoader()).from_string(config('searchFilter'))
+        filter = template.render(data)
+
+
+    else:
+        filter='(&(employeeNumber=' + employeeNumber + ')(employeeType=' + employeeType + '))'
     try:
         r=l.search_s(base,ldap.SCOPE_SUBTREE,filter)
         return r
     except ldap.LDAPError as e:
         e_dict = e.args[0]
-        print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc")))
+        print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc") + " " + e_dict.get("info")))
         exit(1)
 
 def upsert_entry(l,entity):
@@ -170,7 +221,7 @@ def upsert_entry(l,entity):
         #creation de la liste
         entry=make_entry_array_without_empty(entity)
         ## Ajout objectclass
-        entry['objectclass']=make_objectclass(entity)
+        entry['objectclass']=make_objectclass(entity,r)
         dn=compose_dn(entity)
         ldif = modlist.addModlist(convert_to_utf8(entry))
         try:
@@ -178,36 +229,42 @@ def upsert_entry(l,entity):
             l.add_s(dn, ldif)
         except ldap.LDAPError as e:
             e_dict = e.args[0]
-            print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc")))
+            print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc") + " "+ e_dict.get("info")))
             exit(1)
     else:
         if len(r) > 1:
             print(returncode(1,"many identities found"))
             exit(1)
         entry=make_entry_array_without_empty(entity);
-        entry['objectclass'] = make_objectclass(entity)
+        entry=complete_entry(entry,r);
+
+        entry['objectclass'] = make_objectclass(entity,r)
         entry = convert_to_utf8(entry)
-        ldif = modlist.modifyModlist(r[0][1],entry)
+        old_entry=normalize_entry(r[0][1])
+        ldif = modlist.modifyModlist(old_entry,entry)
         dn = compose_dn(entity)
-        if dn == r[0][0]:
+        #dn= r[0][0]
+        if dn_superior(dn) == dn_superior(r[0][0]):
             try:
                 action="mod"
-                l.modify_s(dn,ldif)
+                l.modify_s(r[0][0],ldif)
             except ldap.LDAPError as e:
                 e_dict = e.args[0]
-                print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc")))
+                print(returncode(1, 'mod ' + str(e_dict.get("result")) + ' ' + e_dict.get("desc") + " "+ e_dict.get("info")))
                 exit(1)
         else:
             ## changement du DN
             oldDn=r[0][0]
-            rdn=compose_rdn(entity)
+            rdn=get_rdn_attribure(oldDn)
+            rdnValue=entry[rdn][0].decode('UTF-8')
+            new_rdn=rdn +"="+rdnValue
             newSuperior=dn_superior(compose_dn(entity))
             try:
                 action="rename"
-                l.rename_s(oldDn,rdn,newSuperior)
+                l.rename_s(oldDn,new_rdn,newSuperior)
             except ldap.LDAPError as e:
                 e_dict = e.args[0]
-                print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc")))
+                print(returncode(1, 'rename ' + str(e_dict.get("result")) + ' ' + e_dict.get("desc") + " "+ e_dict.get("info")))
                 exit(1)
     return returncode(0,"Entree " + dn + " " + action)
 
@@ -226,7 +283,7 @@ def change_password(l,uid,old,new):
             l.passwd_s(r[0][0],old,new)
         except ldap.LDAPError as e:
             e_dict = e.args[0]
-            print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc")))
+            print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc") + " "+ e_dict.get("info")))
             exit(1)
     else:
         ldif=[(ldap.MOD_REPLACE, 'userPassword', [new.encode('utf-8')])]
@@ -234,7 +291,7 @@ def change_password(l,uid,old,new):
             l.modify_s(r[0][0], ldif)
         except ldap.LDAPError as e:
             e_dict = e.args[0]
-            print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc")))
+            print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc") + " "+ e_dict.get("info")))
             exit(1)
 
 def change_entity_password(l,entity):
@@ -271,9 +328,31 @@ def delete_entity(l,entity):
             return returncode(0, "user : " + r[0][0] + " deleted")
         except ldap.LDAPError as e:
             e_dict = e.args[0]
-            print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc")))
+            print(returncode(1, str(e_dict.get("result")) + ' ' + e_dict.get("desc") + " "+ e_dict.get("info")))
             exit(1)
     else:
         print(returncode(1, "User not found"))
         exit(1)
+def complete_entry(entry,r):
+    for cle,valeur in r[0][1].items():
+       if cle.lower() != 'objectclass':
+            if cle not in entry:
+                if type(valeur) is list:
+                    for i in valeur:
+                        x=i.decode('UTF-8')
+                    if len(x) == 1:
+                        v=x[0]
+                    else:
+                        v=x
+                    entry[cle.lower()]=v
+    return entry
 
+def normalize_entry(entry):
+    e={}
+    for cle, valeur in entry.items():
+        e[cle.lower()]=valeur
+    return e
+def get_rdn_attribure(dn):
+    dnTab=dn.split(',')
+    attr=dnTab[0].split('=')
+    return attr[0]
